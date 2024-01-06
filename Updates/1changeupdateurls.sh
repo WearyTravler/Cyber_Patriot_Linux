c0785825e5a38c5cf3143533f3bfbc552f0f0513
RedHoodHacker <limbo_shell@protonmail.com>
2024-01-06 01:09:15 -0600

    finished sources.list script

--- Updates/1changeupdateurls.sh
+++ Updates/1changeupdateurls.sh
@@ -1,3 +1,39 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-echo "Adding ubuntu urls to /etc/apt/apt.conf.d/
+echo "Updating sources.list in /etc/apt/"
+sleep 3
+clear
+
+echo "#####ADDED LINES#####" >> /etc/apt/sources.list
+
+echo "deb http://archive.ubuntu.com/ubuntu jammy main restricted" >> /etc/apt/sources.list
+
+echo "deb http://ubuntu.mirror.constant.com jammy main restricted" >> /etc/apt/sources.list
+
+echo "deb http://archive.ubuntu.com/ubuntu jammy-updates main restricted" >> /etc/apt/sources.list
+
+echo "deb http://ubuntu.mirror.constant.com jammy-updates main restricted" >> /etc/apt/sources.list
+
+echo "deb http://archive.ubuntu.com/ubuntu jammy universe" >> /etc/apt/sources.list
+
+echo "deb http://archive.ubuntu.com/ubuntu jammy-updates universe" >> /etc/apt/sources.list
+
+echo "deb http://ubuntu.mirror.constant.com jammy universe" >> /etc/apt/sources.list
+
+echo "deb http://ubuntu.mirror.constant.com jammy-updates universe" >> /etc/apt/sources.list
+
+echo "deb http://archive.ubuntu.com/ubuntu jammy-backports main restricted universe multiverse" >> /etc/apt/sources.list
+
+echo "deb http://ubuntu.mirror.constant.com jammy-backports main restricted universe multiverse" >> /etc/apt/sources.list
+
+echo "deb http://archive.ubuntu.com/ubuntu jammy-security main restricted" >> /etc/apt/sources.list
+
+echo "deb http://archive.ubuntu.com/ubuntu jammy-security universe" >> /etc/apt/sources.list
+
+echo "deb http://archive.ubuntu.com/ubuntu jammy-security multiverse" >> /etc/apt/sources.list
+
+echo "deb http://ubuntu.mirror.constant.com jammy-security main restricted" >> /etc/apt/sources.list
+
+echo "deb http://ubuntu.mirror.constant.com jammy-security universe" >> /etc/apt/sources.list
+
+echo "deb http://ubuntu.mirror.constant.com jammy-security multiverse" >> /etc/apt/sources.list
